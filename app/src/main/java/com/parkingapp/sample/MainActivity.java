9e4ce68d48aa26794976b1b1d1d53b303a44efd3
Omar Sandoval <omar@sandovoid.com>
2015-05-06 14:25:21 -0700

    Implemented the new LocationListener from the FusedLocationApi

--- app/src/main/java/com/parkingapp/sample/MainActivity.java
+++ app/src/main/java/com/parkingapp/sample/MainActivity.java
@@ -18,18 +18,14 @@ package com.parkingapp.sample;
 import android.support.v7.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteException;
 import android.graphics.Color;
 import android.location.Address;
-import android.location.Criteria;
 import android.content.ContextWrapper;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Spannable;
@@ -43,12 +39,14 @@ import android.widget.Toast;
 
 import com.example.pooja.sfparksample.R;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderApi;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -83,38 +81,27 @@ import java.util.zip.GZIPInputStream;
 import android.util.Log;
 
 public class MainActivity extends ActionBarActivity implements
-        LocationListener,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+        LocationListener, ConnectionCallbacks, OnConnectionFailedListener {
 
     private static final String TAG = "LocationActivity";
-    private static final long INTERVAL = 1000 * 10;
-    private static final long FASTEST_INTERVAL = 1000 * 5;
     private static GoogleMap mMap;
-    private LocationManager locationManager;
-    private static final long MIN_TIME = 400;
-    private static final float MIN_DISTANCE = 800;
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
     private String information;
     private String streetCleaningInformation;
-    private FusedLocationProviderApi fusedLocationProviderApi = LocationServices.FusedLocationApi;
-    LocationRequest mLocationRequest;
-    GoogleApiClient mGoogleApiClient;
-    Location mCurrentLocation;
-    String mLastUpdateTime;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mRequestingLocationUpdates = false;
+    private LocationRequest mLocationRequest;
+    private Location mLastLocation;
     MarkerOptions marker;
     List<SFParkBean> SfParkBeanList = null;
     DBConnectionHandler dbConnectionHandler;
 
-    protected void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(INTERVAL);
-        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        createLocationRequest();
+
+        if (checkPlayServices()) {
+            buildGoogleApiClient();
+        }
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
@@ -136,13 +123,7 @@ public class MainActivity extends ActionBarActivity implements
                 .build();
 
         // setup default location onMap load event
-        Criteria criteria = new Criteria();
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
-
-        String provider = locationManager.getBestProvider(criteria, false);
-        Location location = locationManager.getLastKnownLocation(provider);
         double lat = 37.721897;
         double lng = -122.47820939999997;
         LatLng coordinate = new LatLng(lat, lng);
@@ -154,23 +135,61 @@ public class MainActivity extends ActionBarActivity implements
 
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        stopLocationUpdates();
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
     }
 
-    protected void stopLocationUpdates() {
-        // commenting this out for now. causes force close since we are still relying on the old LocationListener
-        //LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Google Play services are not available", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-        super.onResume();
-        setUpMapIfNeeded();
+
+        checkPlayServices();
+
+        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            startLocationUpdates();
+            setUpMapIfNeeded();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
     }
 
 
@@ -717,22 +736,15 @@ public class MainActivity extends ActionBarActivity implements
         // getLastKnownLocation wasn't giving me accurate coordinates.
         // The "new" way to get accurate coordinates is by using the FusedLocationApi.
         if (id == R.id.action_gps) {
-            if (null != mCurrentLocation) {
-                CameraUpdate currentLocation = CameraUpdateFactory.newLatLng(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()));
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (mLastLocation != null) {
+                CameraUpdate currentLocation = CameraUpdateFactory.newLatLng(
+                        new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
                 mMap.animateCamera(currentLocation, 800, null);
             } else {
-                Toast.makeText(getApplicationContext(), "Current location not available.", Toast.LENGTH_LONG).show();
-
-            /*
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            double lat = location.getLatitude();
-            double lng = location.getLongitude();
-            CameraUpdate currentLocation = CameraUpdateFactory.newLatLng(new LatLng(lat, lng));
-            mMap.animateCamera(currentLocation, 800, null);
-            Toast.makeText(getApplicationContext(),
-                    "Current location\nLat: " + lat + "\nLng: " + lng,
-                    Toast.LENGTH_LONG).show();
-            */
+                Toast.makeText(getApplicationContext(),
+                        "Current location not available", Toast.LENGTH_SHORT).show();
             }
         }
         //when user clicks Help-> Street Cleaning Help, help information for Street Cleaning Data will be displayed
@@ -788,42 +800,39 @@ public class MainActivity extends ActionBarActivity implements
 
     @Override
     public void onLocationChanged(Location location) {
-        mCurrentLocation = location;
-        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-    }
+        mLastLocation = location;
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        startLocationUpdates();
+        Toast.makeText(getApplicationContext(), "Location changed!",
+                Toast.LENGTH_SHORT).show();
     }
 
     protected void startLocationUpdates() {
-        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (com.google.android.gms.location.LocationListener) this);
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
     }
+
+    protected void stopLocationUpdates() {
+        // commenting this out for now. causes force close since we are still relying on the old LocationListener
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
 
     @Override
     public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
 
+    @Override
+    public void onConnected(Bundle arg0) {
+        if(mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
     }
 
     /**
